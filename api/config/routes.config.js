5123e583b5d290640f5eab25026c8f07199d82ed
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-05 14:30:12 +0200

    API: Adding endpoint to update User

--- api/config/routes.config.js
+++ api/config/routes.config.js
@@ -1,13 +1,15 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
 
-const { services, user } = require("../controllers") 
+const { services, user } = require("../controllers") ;
 
-router.get("/services", services.getAllServices)
-router.get("/services/:id", services.getService)
-router.post("/services/create", services.createService)
+router.get("/services", services.getAllServices);
+router.get("/services/:id", services.getService);
+router.post("/services/create", services.createService);
 
-router.post("/user/create", user.createUser)
+router.get("/user/:id", user.getUser);
+//router.patch("/user/:id", user.updateUser);
+router.post("/user/create", user.createUser);
 
 
 module.exports = router;
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -2,12 +2,68 @@ const { User } = require("../models");
 
 module.exports.getUser = (req, res, next) => {
   const { id } = req.params;
-  res.json(id);
+  
+  User
+    .findById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        next(createError(404, "El usuario no existe"))
+      }
+    })
+    .catch(next)
 }
 
 module.exports.updateUser = (req, res, next) => {
   const { id } = req.params;
-  res.json(id);
+  const { 
+    email, 
+    password, 
+    nickname, 
+    name, 
+    surname, 
+    phone, 
+    typeStreet, 
+    street, 
+    numberStreet, 
+    floor, 
+    door, 
+    locality, 
+    city, 
+    postcode 
+  } = req.body;
+
+  const user = {
+    email,
+    password,
+    nickname,
+    name,
+    surname,
+    phone,
+    locality, 
+    city, 
+    postcode,
+    address: {
+      typeStreet, 
+      street, 
+      numberStreet, 
+      floor, 
+      door
+    }
+  };
+
+  User
+    .findByIdAndUpdate(id, user, { new: true, runValidators: true })
+    .then(user => {
+      if (user) {
+        res.status(200).json(user)
+      } else {
+        next(createError(404, "El usuario no existe"))
+      }
+    })
+    .catch(next)  
+
 }
 
 module.exports.createUser = (req, res, next) => {
