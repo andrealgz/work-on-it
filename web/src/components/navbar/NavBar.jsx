77ffc30c95435e5bc7b55a58de21a568879c1c53
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-10 12:55:03 +0200

    WEB: Refactoring and fixing bugs

--- web/src/components/navbar/NavBar.jsx
+++ web/src/components/navbar/NavBar.jsx
@@ -28,11 +28,6 @@ function NavBar() {
         </div>
         <nav className={sideBarLeft ? "nav-menu-left active" : "nav-menu-left"}>
           <ul className="nav-menu-items" onClick={showSideBarLeft}>
-            <li className="navbar-toggle">
-              <Link to="#" className="menu-bars" >
-                <AiIcons.AiOutlineClose />
-              </Link>
-            </li>
             {SlideBar.map((item, index) => {
               return (
                 <li key={index} className={item.cName}>
@@ -45,19 +40,24 @@ function NavBar() {
           </ul>
         </nav>
         <nav className={SideBarRight ? "nav-menu-right active" : "nav-menu-right"}>
-          <ul className="nav-menu-items" onClick={showSideBarRight}>
-          <form className="mx-5 text-white mt-5">
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-              <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-              <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-            </div>
-            <div className="mb-3">
-              <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-              <input name="password" type="password" className="form-control" id="exampleInputPassword1" />
-            </div>
-            <button type="submit" className="btn btn-primary">Login</button>
-          </form>
+          <ul className="nav-menu-items">
+            <li className="navbar-toggle">
+              <Link to="#" className="menu-bars" >
+                <AiIcons.AiOutlineClose onClick={showSideBarRight}/>
+              </Link>
+            </li>
+            <form className="mx-5 text-white mt-5">
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
+                <input name="email" type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input name="password" type="password" className="form-control" id="exampleInputPassword1" />
+              </div>
+              <button type="submit" className="btn btn-primary" onClick={showSideBarRight}>Login</button>
+            </form>
           </ul>
         </nav>
       </IconContext.Provider>
--- web/src/components/slide-bar/SlideBar.jsx
+++ web/src/components/slide-bar/SlideBar.jsx
@@ -1,10 +1,56 @@
 import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+import * as FaIcons from "react-icons/fa";
+import * as MdIcons from "react-icons/md";
+import * as GiIcons from "react-icons/gi";
 
 export const SlideBar = [
   {
     title: "Home",
     path: "/",
     icon: <AiIcons.AiFillHome />,
+    cName: "nav-text"
+  },
+  {
+    title: "Fontaneria",
+    path: "/",
+    icon: <IoIcons.IoMdConstruct />,
+    cName: "nav-text"
+  },
+  {
+    title: "Fontaneria",
+    path: "/",
+    icon: <MdIcons.MdPlumbing />,
+    cName: "nav-text"
+  },
+  {
+    title: "Electricista",
+    path: "/",
+    icon: <GiIcons.GiElectric />,
+    cName: "nav-text"
+  },
+  {
+    title: "Carpintero",
+    path: "/",
+    icon: <MdIcons.MdCarpenter />,
+    cName: "nav-text"
+  },
+  {
+    title: "Pintor",
+    path: "/",
+    icon: <AiIcons.AiFillFormatPainter />,
+    cName: "nav-text"
+  },
+  {
+    title: "Bricolaje",
+    path: "/",
+    icon: <FaIcons.FaTools />,
+    cName: "nav-text"
+  },
+  {
+    title: "Mecatrónico",
+    path: "/",
+    icon: <GiIcons.GiMechanicalArm />,
     cName: "nav-text"
   }
 ]
