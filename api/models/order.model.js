b5f2940ae68de34f18b662dd200cd4c8940a7ad3
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-11 19:30:47 +0200

    API: Refactoring orders model

--- api/models/order.model.js
+++ api/models/order.model.js
@@ -11,7 +11,7 @@ const orderSchema = new Schema (
     service: {
       type: Schema.Types.ObjectId,
       required: true,
-      ref: "Services"
+      ref: "Service"
     },
     ownerService: {
       type: Schema.Types.ObjectId,
