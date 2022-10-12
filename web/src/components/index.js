296cd51108998e869150d680de44dd75c4913b5f
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-12 19:28:36 +0200

    WEB: Refactoring services screens and components

--- web/src/components/index.js
+++ web/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as NavBar } from "./header/navbar/NavBar";
 export { default as Login } from "./account/login/Login";
+export { default as DetailService } from "./services/detail/DetailService";
+export { default as ListServices } from "./services/list/ListServices";
