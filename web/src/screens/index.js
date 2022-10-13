c46251921af8cf81c8686f7009080031604c9feb
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-13 21:37:16 +0200

    WEB: User detail component and detail orders component

--- web/src/screens/index.js
+++ web/src/screens/index.js
@@ -4,3 +4,4 @@ export { default as DetailServiceScreen } from "./services/detail/DetailServiceS
 export { default as CreateServiceScreen } from "./services/create/CreateServiceScreen";
 export { default as DetailOrderScreen } from "./orders/detail/DetailOrderScreen";
 export { default as AccountScreen } from "./users/account/AccountScreen";
+export { default as ProfileScreen } from "./users/profile/ProfileScreen";
