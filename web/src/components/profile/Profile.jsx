80f75c89c3d7c21d0f86f794e83c09874c85960d
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-14 14:34:50 +0200

    WEB: Update profile component

--- web/src/components/profile/Profile.jsx
+++ web/src/components/profile/Profile.jsx
@@ -1,7 +1,8 @@
 import * as Services from "../../services/Main";
 import { useParams } from "react-router";
-import { useState, useEffect } from "react";
-import { ListOrder } from "../"
+import { useState, useEffect, useContext } from "react";
+import { ListOrder } from "../";
+import { AccountContext } from "../../contexts/AccountContext";
 
 import "./Profile.css"
 
@@ -10,10 +11,12 @@ function Profile() {
   const [user, setUser] = useState(null);
   const { nickname } = useParams();
 
+  const userConnected = useContext(AccountContext);
+
   useEffect(() => {
     Services
       .getUserProfile(nickname)
-      .then(user => setUser(user))
+      .then(user => setUser(user[0]))
       .catch(error => console.error(error))
   }, [nickname]) 
 
@@ -21,37 +24,38 @@ function Profile() {
     <div className="profile">
       {
         user ? 
-        user.map(user => (
-          <div key={user.id} className="container">
-            <h2 className="fw-bold">{user.nickname}</h2>
-            <div className="row">
-              <div className="col-3">
-                <div className="row">
-                  <div className="col-6 d-flex flex-column">
-                    <label className="text-black-50">Nombre</label>
-                    <span className="fw-bold fs-4">{user.name}</span>
-                  </div>
-                  <div className="col-6 d-flex flex-column">
-                    <label className="text-black-50">Apellidos</label>
-                    <span className="fw-bold fs-4">{user.surname}</span>
-                  </div>
-                  <div className="col-6 d-flex flex-column">
-                    <label className="text-black-50">Ciudad</label>
-                    <span className="fw-bold fs-4">{user.address}</span>
-                  </div>
-                  <div className="col-6 d-flex flex-column">
-                    <label className="text-black-50">Localidad</label>
-                    <span className="fw-bold fs-4">{user.locality}</span>
-                  </div>
+        <div className="container">
+          <h2 className="fw-bold">{user.nickname}</h2>
+          <div className="row">
+            <div className="col-3">
+              <div className="row">
+                <div className="col-6 d-flex flex-column">
+                  <label className="text-black-50">Nombre</label>
+                  <span className="fw-bold fs-4">{user.name}</span>
+                </div>
+                <div className="col-6 d-flex flex-column">
+                  <label className="text-black-50">Apellidos</label>
+                  <span className="fw-bold fs-4">{user.surname}</span>
+                </div>
+                <div className="col-6 d-flex flex-column">
+                  <label className="text-black-50">Ciudad</label>
+                  <span className="fw-bold fs-4">{user.address}</span>
+                </div>
+                <div className="col-6 d-flex flex-column">
+                  <label className="text-black-50">Localidad</label>
+                  <span className="fw-bold fs-4">{user.locality}</span>
                 </div>
               </div>
+            </div>
+            {
+              user.id === userConnected.user.id &&
               <div className="col-9 d-flex">
                 <ListOrder listOrders={user.orderSent} className="mx-3"/>
                 <ListOrder listOrders={user.orderReceived} className="mx-3"/>
               </div>
-            </div>
+            }
           </div>
-        )) : 
+        </div> : 
         <p>Loading....</p>
       }
     </div>
