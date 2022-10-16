1b8148531a870c8de016e8937ebf4ab70f37e805
Andrea <andrea_lgz@hotmail.com>
2022-10-16 11:28:35 +0200

    WEB:ListServices interface

--- web/src/components/services/list/ListServices.jsx
+++ web/src/components/services/list/ListServices.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { translation } from "../../../utils/translation";
 
+import "./ListServices.css";
 import * as Services from "../../../services/Main";
 
 function ListServices() {
@@ -19,28 +20,49 @@ function ListServices() {
   },[profession])
 
   return (
-    <div className="container mt-5">
-      <div className="row">
-        {
-          services ? 
-            services.length ? 
-              services.map(service => (
-              <div key={service.id} className="col-4">
-                <Link to={`/service/${service.id}`} >
-                  <div className="card">
-                    <img src="https://cflvdg.avoz.es/sc/VhmlADGQ6fh-z3-ezqRYiN6anZM=/480x/2022/02/09/00121644411841910536634/Foto/L01F2017.jpg" className="card-img-top" alt="..." />
-                    <div className="card-body">
-                      <h5 className="card-title">{service.user?.nickname}</h5>
-                      <p className="card-text">{translation("experiences", service.experience)}</p>
-                      <p className="card-text">{service.rating}</p>
-                    </div>
+    <div>
+      <div className="title-services d-flex justify-content-center">Profesionales</div>
+      <div className="filterbox d-flex justify-content-flex-start">
+        <div>
+          <h6>Especialidad <img src="https://www.tikamoon.es/build/images/search/slide-picto.01a5a605.svg" alt="services"/></h6>
+        </div>
+        <div>
+          <h6>Precio <img src="https://www.tikamoon.es/build/images/search/slide-picto.01a5a605.svg" alt="services"/></h6>
+        </div>
+        <div>
+          <h6>Experiencia <img src="https://www.tikamoon.es/build/images/search/slide-picto.01a5a605.svg" alt="services"/></h6>
+        </div>
+        <div>
+          <h6>Valoración <img src="https://www.tikamoon.es/build/images/search/slide-picto.01a5a605.svg" alt="services"/></h6>
+        </div>
+      </div>
+      <div className="services d-flex justify-content-center">
+        <div className="row">
+          {
+            services ? 
+              services.length ? 
+                services.map(service => (
+                  <div key={service.id} className="col-3">
+                      <Link to={`/service/${service.id}`} style={{ color: 'inherit', textDecoration: 'inherit'}}>
+                        <div>
+                          <div className="profile-card">
+                            <img src="https://cflvdg.avoz.es/sc/VhmlADGQ6fh-z3-ezqRYiN6anZM=/480x/2022/02/09/00121644411841910536634/Foto/L01F2017.jpg" alt="image1" className="profile-icon" />
+                            <h4 className="nickname">{service.user?.nickname}</h4>
+                            <h5><div>{translation("professions", service.profession)}</div></h5>
+                            <div className="details">
+                              <div className="price">Precio/hora: {service.rate}€</div>
+                              <div className="price">Experiencia: {translation("experiences", service.experience)}</div>
+                              <div>Valoración:{service.rating}</div>
+                            </div>
+                          </div>
+                        </div>  
+                      </Link>
                   </div>
-                </Link>
-              </div>
-              )) :
+                )) :
               <div className="col-12 text-center"><p>No hay ningun servicio</p></div>
-          : <p>Cargando...</p>
-        }
+            : <p>Cargando...</p>
+          } 
+        </div>
       </div>
     </div>
   )
@@ -48,3 +70,12 @@ function ListServices() {
 }
 
 export default ListServices;
+
+
+{/* <img src="https://cflvdg.avoz.es/sc/VhmlADGQ6fh-z3-ezqRYiN6anZM=/480x/2022/02/09/00121644411841910536634/Foto/L01F2017.jpg" classNameName="card-img-top" alt="img" />
+{service.user?.nickname}
+{translation("professions", service.profession)}
+{translation("experiences", service.experience)}
+{service.rating}
+{service.rate}
+{translation("timeTables", service.timeTable)} */}
