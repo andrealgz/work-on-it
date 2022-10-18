ac1fbe7a4b895b60c44bd7034241f7e1b21fc185
Andrea <andrea_lgz@hotmail.com>
2022-10-18 20:33:55 +0200

    WEB: carousel Home update and images upload

--- web/src/screens/home/HomeScreen.jsx
+++ web/src/screens/home/HomeScreen.jsx
@@ -1,11 +1,12 @@
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import "./HomeScreen.css";
 
 function HomeScreen() {
   return (
     <>
       <Carousel 
-          className="home-screen-carousel"
+          className="home-screen-carousel h-100"
           interval={7500}
           autoPlay={true}
           showArrows={false}
@@ -16,10 +17,9 @@ function HomeScreen() {
           stopOnHover={false}
           axis='vertical'
         >
-          <img src="https://www.adra360.com/wp-content/uploads/2020/07/reparaciones-hogar-adra360.jpg" alt="img1"/>
-          <img src="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="img2"/>
-          <img src="https://www.adra360.com/wp-content/uploads/2020/07/reparaciones-hogar-adra360.jpg" alt="img1"/>
-          <img src="https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="img2"/>
+          <div className="home-img img1 h-100"></div>
+          <div className="home-img img2 h-100"></div>
+          <div className="home-img img3 h-100"></div>
       </Carousel>
     </>
   )
