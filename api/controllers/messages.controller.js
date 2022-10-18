82cb606844e94f29e04fea2991eb9277c6085ba2
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-18 21:20:20 +0200

    API/WEB: Update message controller

--- api/controllers/messages.controller.js
+++ api/controllers/messages.controller.js
@@ -2,10 +2,16 @@ const { Message } = require("../models");
 const createError = require("http-errors")
 
 module.exports.setMessage = (req, res, next) => {
-  const { sender, receiver, service, message } = req.body;
+  const { order, message } = req.body;
   const { id } = req.params;
-  const communication = { sender, receiver, order: id, service, message };
 
+  const communication = { 
+    sender: req.user.id, 
+    receiver: req.user.id === order.customer.id ? order.ownerService.id : order.customer.id, 
+    order: id, 
+    service: order.service.id, 
+    message 
+  };
 
   Message
     .create(communication)
