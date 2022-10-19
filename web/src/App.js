778a679b4c58187645ac2476baa21786eb5d67f2
CrisArranz <c.arranz.arevalo@gmail.com>
2022-10-19 12:21:05 +0200

    WEB: Update Navbar with js to hide both sidebars

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,24 @@
-import "./App.css"
-import NavigationRoutes from "./navigation/NavigationRoutes"
-import { NavBar } from "./components"
+import "./App.css";
+import NavigationRoutes from "./navigation/NavigationRoutes";
+import { NavBar } from "./components";
+import { useState } from "react";
 
 function App() {
+  const [sideBarLeft, setSidebarLeft] = useState(false);
+  const [SideBarRight, setSidebarRight] = useState(false);
+
+  const showSideBarLeft = (status = undefined) => setSidebarLeft(status === undefined ? !sideBarLeft : status);
+  const showSideBarRight = (status = undefined) => setSidebarRight(status === undefined ? !SideBarRight : status);
+
   return (
     <>
-      <NavBar />
-      <NavigationRoutes />
+      <NavBar stateLeft={sideBarLeft} onClickLeft={showSideBarLeft} stateRight={SideBarRight} onClickRight={showSideBarRight}/>
+      <div className="navigation-routes h-100" onClick={() => {
+        showSideBarLeft(false);
+        showSideBarRight(false);
+      }}>
+        <NavigationRoutes />
+      </div>
     </>  
   );
 }
