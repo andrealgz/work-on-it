98e873c63d08a1d2d24425d82193153bb86ac596
Andrea <andrea_lgz@hotmail.com>
2022-10-22 15:53:58 +0200

    WEB: design screens

--- web/src/components/orders/detail/DetailOrder.jsx
+++ web/src/components/orders/detail/DetailOrder.jsx
@@ -74,139 +74,139 @@ function DetailOrderScreen() {
             <div className="row justify-content-center align-content-center">
               <div className="col-9">
                 <div className="row">
-                <div className="col-3 d-flex flex-column">
-                  <div className="row border border-2 rounded p-4">
-                    <h3>Solicitante</h3>
-                    <div className="col-6 d-flex flex-column">
-                      <img className="rounded-circle" width="100" src={order.customer.photo} alt={order.customer.nickname} />
-                    </div>
-                    <div className="col-6 d-flex flex-column">
-                      <label className="text-black-50">Nickname</label>
-                      <span className="fw-bold fs-4">{order.customer.id === user.id ? <Link to={"/users/me"}>{order.customer.nickname}</Link> : order.customer.nickname}</span>
-                    </div>
-                    <div className="col-6 d-flex flex-column">
-                      <label className="text-black-50">Nombre</label>
-                      <span className="fw-bold fs-4">{order.customer.name}</span>
-                      <label className="text-black-50">Apellido</label>
-                      <span className="fw-bold fs-4">{order.customer.surname}</span>
-                    </div>
-                    <div className="col-6 d-flex flex-column">
-                      <label className="text-black-50">Localidad</label>
-                      <span className="fw-bold fs-4">{order.customer.locality}</span>
-                    </div>
-                  </div>
-                  <div className="row border border-2 rounded p-4 mt-2">
-                    <h3 className="mt-3">Propietario del servicio</h3>
-                    <div className="col-6 d-flex flex-column">
-                      <img className="rounded-circle" width="100" src={order.ownerService.photo} alt={order.ownerService.nickname} />
-                    </div>
-                    <div className="col-6 d-flex flex-column">
-                      <label className="text-black-50">Nickname</label>
-                      <span className="fw-bold fs-4">{order.ownerService.id === user.id ? <Link to={"/users/me"}>{order.ownerService.nickname}</Link> : order.ownerService.nickname}</span>
-                    </div>
-                    <div className="col-6 d-flex flex-column">
-                      <label className="text-black-50">Nombre</label>
-                      <span className="fw-bold fs-4">{order.ownerService.name}</span>
-                      <label className="text-black-50">Apellido</label>
-                      <span className="fw-bold fs-4">{order.ownerService.surname}</span>
-                    </div>
-                    <div className="col-6 d-flex flex-column">
-                      <label className="text-black-50">Localidad</label>
-                      <span className="fw-bold fs-4">{order.ownerService.locality}</span>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-9">
-                  <div className="row">
-                    <div className="col-6">
-                      <div className="d-flex border border-2 rounded p-4 justify-content-around">
-                        <div className="d-flex flex-column justify-content-between">
-                          <h3>Factura del encargo</h3>
-                          <span>Precio: {order.detailJob.rate}€</span>
-                          <span>Horas contratadas: {order.detailJob.hours}</span>
-                          <h3>Total: {order.detailJob.rate * order.detailJob.hours}€</h3>
-                        </div>
-                        <img className="rounded-circle" width="150" src="https://res.cloudinary.com/dc7llr1ic/image/upload/v1666425746/work-on-it/Logo_completo_ijgotg.png" alt="Logo Work On It" />
+                  <div className="col-3 d-flex flex-column">
+                    <div className="detail-order-box row border border-2 rounded p-4">
+                      <h3>Solicitante</h3>
+                      <div className="col-6 d-flex flex-column">
+                        <img className="rounded-circle" width="100" src={order.customer.photo} alt={order.customer.nickname} />
+                      </div>
+                      <div className="col-6 d-flex flex-column">
+                        <label className="text-black-50">Nickname</label>
+                        <span className="fw-bold fs-4">{order.customer.id === user.id ? <Link to={"/users/me"}>{order.customer.nickname}</Link> : order.customer.nickname}</span>
+                      </div>
+                      <div className="col-6 d-flex flex-column">
+                        <label className="text-black-50">Nombre</label>
+                        <span className="fw-bold fs-4">{order.customer.name}</span>
+                        <label className="text-black-50">Apellido</label>
+                        <span className="fw-bold fs-4">{order.customer.surname}</span>
+                      </div>
+                      <div className="col-6 d-flex flex-column">
+                        <label className="text-black-50">Localidad</label>
+                        <span className="fw-bold fs-4">{order.customer.locality}</span>
                       </div>
                     </div>
-                    <div className="col-6 d-flex align-items-center">
-                      {
-                        order.status !== "finish" ?
-                          order.ownerService.id === user.id ? 
-                          <form onSubmit={handleSubmit(handleUpdateOrder)}>
-                            <Controller 
-                              name="status" 
-                              control={control}
-                              defaultValue={order?.status}
-                              render={({ field: { onBlur, onChange, value } }) => (
-                                <div className="d-flex">
-                                  <label className="d-flex align-items-center me-3">Estado: </label>
-                                  <Select className="form-control p-0 select-status-order"
-                                    value={status.find(status => status.value === value)} 
-                                    onChange={status => onChange(status)} 
-                                    onBlur={onBlur}
-                                    options={status}
-                                    styles={{
-                                      control: (base) => ({
-                                        ...base,
-                                        border: 0
-                                      })
-                                    }}/>
-                                  {errors.status && (<div className="invalid-feedback">{errors.status.message}</div>)}
-                                  <button className="ms-1 btn btn-primary" type="submit"><IconsTi.TiTick /></button>
-                                </div>
-                              )}
-                            />
-                          </form> :
-                          <div className="d-flex">
-                            <label className="d-flex align-items-center me-3">Estado: </label>
-                            <span className="fw-bold fs-4">{translation("status", order.status ).toUpperCase()}</span>
-                          </div>
-                          :
-                          <div className="d-flex">
-                            <label className="d-flex align-items-center me-3">Estado: </label>
-                            <span className="fw-bold fs-4">TERMINADO</span>
-                          </div>
-                        }
+                    <div className="detail-order-box row border border-2 rounded p-4 mt-2">
+                      <h3 className="mt-3">Propietario del servicio</h3>
+                      <div className="col-6 d-flex flex-column">
+                        <img className="rounded-circle" width="100" src={order.ownerService.photo} alt={order.ownerService.nickname} />
+                      </div>
+                      <div className="col-6 d-flex flex-column">
+                        <label className="text-black-50">Nickname</label>
+                        <span className="fw-bold fs-4">{order.ownerService.id === user.id ? <Link to={"/users/me"}>{order.ownerService.nickname}</Link> : order.ownerService.nickname}</span>
+                      </div>
+                      <div className="col-6 d-flex flex-column">
+                        <label className="text-black-50">Nombre</label>
+                        <span className="fw-bold fs-4">{order.ownerService.name}</span>
+                        <label className="text-black-50">Apellido</label>
+                        <span className="fw-bold fs-4">{order.ownerService.surname}</span>
+                      </div>
+                      <div className="col-6 d-flex flex-column">
+                        <label className="text-black-50">Localidad</label>
+                        <span className="fw-bold fs-4">{order.ownerService.locality}</span>
+                      </div>
                     </div>
                   </div>
-                  <div className="row">
-                    <div className="col-12">
-                      <div className={`detail-order-message-panel border border-2 rounded mb-1 d-flex flex-column ${!order.messages.length ? "justify-content-center align-items-center" : ""}`}>
+                  <div className="col-9">
+                    <div className="row">
+                      <div className="col-6">
+                        <div className="detail-order-box d-flex border border-2 rounded p-4 justify-content-around">
+                          <div className="d-flex flex-column justify-content-between">
+                            <h3>Factura del encargo</h3>
+                            <span>Precio: {order.detailJob.rate}€</span>
+                            <span>Horas contratadas: {order.detailJob.hours}</span>
+                            <h3>Total: {order.detailJob.rate * order.detailJob.hours}€</h3>
+                          </div>
+                          <img className="rounded-circle" width="150" src="https://res.cloudinary.com/dc7llr1ic/image/upload/v1666425746/work-on-it/Logo_completo_ijgotg.png" alt="Logo Work On It" />
+                        </div>
+                      </div>
+                      <div className="col-6 d-flex align-items-center">
                         {
-                          order.messages.length ? 
-                          order.messages.map((message, index) => 
-                            (
-                              <div key={message.id} className={`d-flex flex-column align-items-${message.sender.nickname !== user.nickname ? "start" : "end"}`}>
-                                <div className={`d-flex ${message.sender.nickname !== user.nickname ? "flex-row-reverse" : ""}`}>
-                                  <img className="rounded-circle mt-2" width="35" src={message.sender.photo} alt={message.sender.nickname} />
-                                  <span className={`rounded-pill d-flex align-items-center ${message.sender.nickname !== user.nickname ? "bg-primary" : "bg-success"} bg-opacity-25 px-3 mx-2 mt-2`} key={`message-${index}`}>
-                                    {message.message}
-                                  </span>
-                                </div>
-                                <span className="detail-order-message-date px-3 mx-2" key={`date-${index}`}>{new Date(message.createdAt).toLocaleString()}</span>
-                              </div>
-                            )
-                          ) : 
-                          <span>No tienes ningun mensaje</span>
-                        }
+                          order.status !== "finish" ?
+                            order.ownerService.id === user.id ? 
+                            <form onSubmit={handleSubmit(handleUpdateOrder)}>
+                              <Controller 
+                                name="status" 
+                                control={control}
+                                defaultValue={order?.status}
+                                render={({ field: { onBlur, onChange, value } }) => (
+                                  <div className="d-flex">
+                                    <label className="d-flex align-items-center me-3">Estado: </label>
+                                    <Select className="form-control p-0 select-status-order"
+                                      value={status.find(status => status.value === value)} 
+                                      onChange={status => onChange(status)} 
+                                      onBlur={onBlur}
+                                      options={status}
+                                      styles={{
+                                        control: (base) => ({
+                                          ...base,
+                                          border: 0
+                                        })
+                                      }}/>
+                                    {errors.status && (<div className="invalid-feedback">{errors.status.message}</div>)}
+                                    <button className="ms-1 btn btn-primary" type="submit"><IconsTi.TiTick /></button>
+                                  </div>
+                                )}
+                              />
+                            </form> :
+                            <div className="d-flex">
+                              <label className="d-flex align-items-center me-3">Estado: </label>
+                              <span className="fw-bold fs-4">{translation("status", order.status ).toUpperCase()}</span>
+                            </div>
+                            :
+                            <div className="d-flex">
+                              <label className="d-flex align-items-center me-3">Estado: </label>
+                              <span className="fw-bold fs-4">TERMINADO</span>
+                            </div>
+                          }
                       </div>
-                      <form onSubmit={handleSubmit(handleMessage)}>
-                        <div className="input-group mb-1">
-                          <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Mensaje"
-                            disabled={order.status === "finish"}
-                            {...register("message")}
-                          />
-                          <button className="ms-1 btn btn-primary" disabled={order.status === "finish"} type="submit"><IconsGi.GiEnvelope /></button>
+                    </div>
+                    <div className="row">
+                      <div className="col-12">
+                        <div className={`detail-order-message-panel border border-2 rounded mb-1 d-flex flex-column ${!order.messages.length ? "justify-content-center align-items-center" : ""}`}>
+                          {
+                            order.messages.length ? 
+                            order.messages.map((message, index) => 
+                              (
+                                <div key={message.id} className={`d-flex flex-column align-items-${message.sender.nickname !== user.nickname ? "start" : "end"}`}>
+                                  <div className={`d-flex ${message.sender.nickname !== user.nickname ? "flex-row-reverse" : ""}`}>
+                                    <img className="rounded-circle mt-2" width="35" src={message.sender.photo} alt={message.sender.nickname} />
+                                    <span className={`rounded-pill d-flex align-items-center ${message.sender.nickname !== user.nickname ? "bg-primary" : "bg-success"} bg-opacity-25 px-3 mx-2 mt-2`} key={`message-${index}`}>
+                                      {message.message}
+                                    </span>
+                                  </div>
+                                  <span className="detail-order-message-date px-3 mx-2" key={`date-${index}`}>{new Date(message.createdAt).toLocaleString()}</span>
+                                </div>
+                              )
+                            ) : 
+                            <span>No tienes ningun mensaje</span>
+                          }
                         </div>
-                      </form>
+                        <form onSubmit={handleSubmit(handleMessage)}>
+                          <div className="input-group mb-1">
+                            <input
+                              type="text"
+                              className="form-control"
+                              placeholder="Mensaje"
+                              disabled={order.status === "finish"}
+                              {...register("message")}
+                            />
+                            <button className="ms-1 btn btn-primary" disabled={order.status === "finish"} type="submit"><IconsGi.GiEnvelope /></button>
+                          </div>
+                        </form>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
               </div>
             </div>
             {
