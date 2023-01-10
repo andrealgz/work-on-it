8f8b70d2b6b5a8e241f0a394acba2d358c7024ed
CrisArranz <c.arranz.arevalo@gmail.com>
2023-01-10 17:55:20 +0100

    API: remove CORS policy, not neccesary

--- api/app.js
+++ api/app.js
@@ -8,13 +8,13 @@ require('./config/db.config');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.set("Access-Control-Allow-Headers", "content-type");
-  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.set("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.set("Access-Control-Allow-Headers", "content-type");
+//   res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   res.set("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use(express.static(`${__dirname}/react-app`));
 
